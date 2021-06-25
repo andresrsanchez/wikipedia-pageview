ac0e8672975ad635ca8c5d4ca4b2003aabdd48b9
Andrés Rodríguez Sánchez <arodriguez@plainconcepts.com>
2021-06-25 18:27:57 +0200

    feat: first working implementation for full pipeline
    First working implementation from the download of the wikimedia dump
    to writing results to a file, with a priorityqueue and stack as
    main collections. Also we didn't invest in writing tests yet
    because of the lack of knowledge in golang.
    
    Now looking for tests, channels and goroutines.

--- processor.go
+++ processor.go
@@ -1,22 +1,133 @@
 package main
 
 import (
+	"bufio"
+	"compress/gzip"
+	"container/heap"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
+const CAPACITY = 25
+
 func main() {
-	valid, from, to := validateAndGet("yyyy-MM-ddTHH:00:00", "2020-01-01T00:00:00", "2020-01-02T00:00:00")
+	valid, from, to := validateAndGet("yyyy-MM-ddTHH:00:00", "2020-01-01T01:00:00", "2020-01-02T02:00:00")
 	if !valid {
 		panic(fmt.Errorf("there is an error in dateFrom: %s or dateTo: %s", from, to))
 	}
 
 	blackList := getBlackList()
+	path := download(from)
+	results := consumeFileAndReturnResults(path, blackList)
+	writeResultsToFile("20200101-000000", results)
+}
 
+func download(date time.Time) string {
+	url := fmt.Sprintf("https://dumps.wikimedia.org/other/pageviews/%v/%v/pageviews-%v-%s0000.gz",
+		date.Year(), date.String()[0:7], date.Format("20060102"), date.Format("15"))
+	fmt.Println(url)
+	resp, err := http.Get(url)
+	if err == nil && resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("there is an error getting the blacklist with statusCode: %v", resp.StatusCode)
+	}
+	check(err)
+	defer resp.Body.Close()
+
+	dir, err := ioutil.TempDir("", "")
+	check(err)
+
+	out, err := ioutil.TempFile(dir, "")
+	check(err)
+	fmt.Println(out.Name())
+
+	defer out.Close()
+	io.Copy(out, resp.Body)
+
+	// err = os.Remove(dir)
+	// check(err)
+
+	return out.Name()
+}
+
+func consumeFileAndReturnResults(path string, blackList map[string]map[string]bool) map[string]PriorityQueue {
+	top := make(map[string]PriorityQueue)
+
+	file, err := os.Open(path)
+	check(err)
+
+	gz, err := gzip.NewReader(file)
+	check(err)
+
+	defer file.Close()
+	defer gz.Close()
+
+	scanner := bufio.NewScanner(gz)
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+		domain := line[0]
+		title := line[1]
+
+		if _, ok := blackList[domain][title]; ok {
+			continue
+		}
+		views, _ := strconv.Atoi(line[2])
+
+		if sortedValues, ok := top[domain]; ok {
+			if sortedValues.Len() < CAPACITY {
+				heap.Push(&sortedValues, &Item{
+					priority: views,
+					value:    title,
+				})
+				top[domain] = sortedValues
+			} else if val, ok := sortedValues.Peek(); ok == nil && views > val.priority {
+				heap.Pop(&sortedValues)
+				heap.Push(&sortedValues, &Item{
+					priority: views,
+					value:    title,
+				})
+				top[domain] = sortedValues
+			}
+		} else {
+			sortedValues = make(PriorityQueue, 0, CAPACITY)
+			heap.Push(&sortedValues, &Item{
+				priority: views,
+				value:    title,
+			})
+			top[domain] = sortedValues
+		}
+	}
+	return top
+}
+
+func writeResultsToFile(date string, results map[string]PriorityQueue) {
+	err := os.MkdirAll("/dumps", os.ModePerm)
+	check(err)
+
+	var b strings.Builder
+	for k, v := range results {
+		stack := make(Stack, 0, v.Len())
+		for v.Len() > 0 {
+			element := heap.Pop(&v).(*Item)
+			stack = stack.Push(fmt.Sprintf("%s %s %v", k, element.value, element.priority))
+		}
+		for len(stack) > 0 {
+			var result string
+			stack, result = stack.Pop()
+			fmt.Fprintf(&b, "%s \n", result)
+		}
+		fmt.Printf("processing domain: %s \n", k)
+	}
+
+	err = ioutil.WriteFile(fmt.Sprintf("/dumps/%s", date), []byte(b.String()), 0644)
+	check(err)
 }
 
 func getBlackList() map[string]map[string]bool {
@@ -43,8 +154,8 @@ func getBlackList() map[string]map[string]bool {
 		if len(splittedLine) != 2 {
 			check(fmt.Errorf("there is and error with the splittedLine: %v", splittedLine))
 		}
-		titles := blackList[splittedLine[0]]
-		if len(titles) == 0 {
+
+		if titles := blackList[splittedLine[0]]; len(titles) == 0 {
 			blackList[splittedLine[0]] = map[string]bool{splittedLine[1]: true}
 		} else {
 			containsTitle := titles[splittedLine[1]]
@@ -53,7 +164,6 @@ func getBlackList() map[string]map[string]bool {
 			}
 		}
 	}
-
 	return blackList
 }
 
@@ -62,10 +172,6 @@ func check(e error) {
 		panic(e)
 	}
 }
-
-// func ProcessAndGetResultsFilePath(dateFrom string, dateTo string) {
-
-// }
 
 func validateAndGet(format string, from string, to string) (bool, time.Time, time.Time) {
 	parse := func(dateTime string) time.Time {
